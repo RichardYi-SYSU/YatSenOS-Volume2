439d170e965b6e3c68306351757b851b2f512cc7
RichardYi <1641687849@qq.com>
2026-03-25 20:21:20 +0800

    完成时钟中断相关实验任务代码的编写

---
+++ crates/kernel/src/interrupt/clock.rs
@@ -0,0 +1,32 @@
+use core::sync::atomic::{AtomicU64, Ordering};
+
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+
+use super::consts::*;
+
+static COUNTER: AtomicU64 = AtomicU64::new(0);
+
+pub unsafe fn register_idt(idt: &mut InterruptDescriptorTable) {
+    idt[Interrupts::IrqBase as u8 + Irq::Timer as u8].set_handler_fn(clock_handler);
+}
+
+pub extern "x86-interrupt" fn clock_handler(_sf: InterruptStackFrame) {
+    x86_64::instructions::interrupts::without_interrupts(|| {
+        if inc_counter() % 0x10000 == 0 {
+            info!("Tick! @{}", read_counter());
+        }
+        super::ack();
+    });
+}
+
+#[inline]
+pub fn read_counter() -> u64 {
+    // FIXME: load counter value
+    COUNTER.load(Ordering::Relaxed)
+}
+
+#[inline]
+pub fn inc_counter() -> u64 {
+    // FIXME: read counter value and increase it
+    COUNTER.fetch_add(1, Ordering::Relaxed) + 1
+}
